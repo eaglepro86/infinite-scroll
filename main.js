6cf8acd1bc307db8d069010444fcfde2a636745a
adam <eaglepro86@icloud.com>
2021-04-09 23:05:48 +0100

    updated api

--- main.js
+++ main.js
@@ -8,14 +8,33 @@ const count = 10;
 const apiKey = '6AAzqEWxAIJOeMCrN7K0SdtaHsotyXGqvkpzJnKYemk';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create elements for links and photos & add to DOM
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+
+        // craete anchor element to link to unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+
+        // create image for photo
+        const img  = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    })
+}
 
 // get photos from unsplash api
 async function getPhotos() {
     try {
 
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos();
 
     } catch(error) {
         console.log(error)
