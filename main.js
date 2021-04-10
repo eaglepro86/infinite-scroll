25f78f4432d279ec35a655e706b86cf22fed759f
adam <eaglepro86@icloud.com>
2021-04-10 22:22:25 +0100

    full api added and DRY code removed

--- main.js
+++ main.js
@@ -8,30 +8,50 @@ const count = 10;
 const apiKey = '6AAzqEWxAIJOeMCrN7K0SdtaHsotyXGqvkpzJnKYemk';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+   for (const key in attributes) {
+       element.setAttributes(key, attributes[key]);
+   } 
+}
+
 // Create elements for links and photos & add to DOM
 function displayPhotos() {
+
+    // run function for each object in photoArray
+    // (photo) is a variable, each object{} will be assigned to this variable
     photosArray.forEach((photo) => {
 
-        // craete anchor element to link to unsplash
+        // create anchor element to link to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.link.html,
+            target: '_blank',
+        });
 
-        // create image for photo
+        // create image element
         const img  = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
 
+        // // set the image source
+        setAttributes(img, {
+            src: photo.urls.regurlar,
+            alt: photo.alt_description,
+            title: photo.alt_description, 
+        })
+
+        // Put <img> inside of <a> then put both inside our imageContainer
+        // add a child to item
         item.appendChild(img);
+
+        // add the item as a child to image-container
         imageContainer.appendChild(item);
     })
 }
 
 // get photos from unsplash api
 async function getPhotos() {
-    try {
 
+    try {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos();
